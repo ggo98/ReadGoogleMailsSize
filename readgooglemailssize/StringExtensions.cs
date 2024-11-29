9ba0d5a6bd5750d9615083655e998b2c021a7669
Olivier gg <ggo98@outlook.com>
2024-11-29 12:14:26 +0100

    minor changes 2

---
+++ readgooglemailssize/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readgooglemailssize
+{
+    public static class StringExtensions
+    {
+        public static string SmartSubString(this string s, int startIndex, int length)
+        {
+            int slen = s.Length;
+            if (startIndex > slen)
+                return string.Empty;
+            int len = slen - startIndex;
+            if (len > length)
+                len = length;
+            if (len < 0)
+                return string.Empty;
+            string ret = s.Substring(startIndex, len);
+            return ret;
+        }
+    }
+}
