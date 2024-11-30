b2019a5187a23118a1b395cf3cbda4487b1970c7
Olivier gg <ggo98@outlook.com>
2024-11-30 15:53:44 +0100

    read message labels

---
+++ readgooglemailssize/MailMessageInformation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace readgooglemailssize
+{
+    internal class MailMessageInformation
+    {
+        public string Subject { get; set; }
+        public string From { get; set; }
+        public string To { get; set; }
+        public DateTime Date { get; set; }
+        public Int64 Size { get; set; }
+        public string Snippet { get; set; }
+        public string Labels { get; set; }
+    }
+}
